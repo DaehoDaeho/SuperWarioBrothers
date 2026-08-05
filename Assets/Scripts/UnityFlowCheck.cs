2b56a883a5b0d489c771975f3d0b1e532150324c
DaehoDaeho <relayer@naver.com>
2026-08-05 21:44:01 +0900

    Initial commit

---
+++ Assets/Scripts/UnityFlowCheck.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class UnityFlowCheck : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake가 실행되었습니다.");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start가 실행되었습니다.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Update가 실행 중입니다.");
+    }
+}
