90be10d1e1bb99c3f4e07c71aa2faee2075014b1
DaehoDaeho <relayer@naver.com>
2026-08-13 21:42:02 +0900

    .

--- Assets/Scripts/Player/PlayerKeyboardMove.cs
+++ Assets/Scripts/Player/PlayerKeyboardMove.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 public class PlayerKeyboardMove : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
+    public float jumpPower = 10.0f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
     Rigidbody2D body;
     float xDirection;
+
+    bool jumpPressed;
+    bool isGrounded;
 
     private void Awake()
     {
@@ -28,11 +36,31 @@ public class PlayerKeyboardMove : MonoBehaviour
         {
             xDirection = -1.0f;
         }
+
+        if(Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            jumpPressed = true;
+        }
     }
 
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
         float xSpeed = xDirection * moveSpeed;
         body.linearVelocity = new Vector2(xSpeed, body.linearVelocity.y);
+
+        if(jumpPressed == true && isGrounded == true)
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpPower);
+        }
+
+        jumpPressed = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
